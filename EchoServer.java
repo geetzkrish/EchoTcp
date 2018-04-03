f30883044da0b779eb0935814602bc31e72aecdf
geet <geeethu94@gmail.com>
2018-04-03 13:42:04 +0530

    add keyread

--- EchoServer.java
+++ EchoServer.java
@@ -8,3 +8,8 @@ public class EchoServer
       ServerSocket sersock = new ServerSocket(3000);
       System.out.println("Server ready for chatting");
       Socket sock = sersock.accept( );   
+      // reading from keyboard (keyRead object)
+      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+	  // sending to client (pwrite object)
+      OutputStream ostream = sock.getOutputStream(); 
+      PrintWriter pwrite = new PrintWriter(ostream, true);
